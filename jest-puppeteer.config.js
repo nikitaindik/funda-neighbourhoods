4529296218334e10b88abc2d6718bf9db3f0cd9d
Nikita Indik <mail@nikitaindik.com>
2020-03-08 19:47:23 +0300

    ...

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -5,6 +5,7 @@ const pathToExtension = path.resolve("./build");
 module.exports = {
   launch: {
     headless: false,
+    executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
     args: [
       `--disable-extensions-except=${pathToExtension}`,
       `--load-extension=${pathToExtension}`

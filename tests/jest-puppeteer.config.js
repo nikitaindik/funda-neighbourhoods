76c2834b1228cac93fd43eab43e169699eda72aa
Nikita Indik <thevorpall@gmail.com>
2022-03-30 13:44:51 +0200

    .

--- tests/jest-puppeteer.config.js
+++ tests/jest-puppeteer.config.js
@@ -1,15 +1,16 @@
 const path = require("path");
+const fs = require("fs");
 
 const pathToExtension = path.resolve("./build");
+fs.readdir(pathToExtension, (error, files) => {
+  console.log(files);
+});
 
 module.exports = {
   launch: {
     headless: false,
-    args: [
-      `--disable-extensions-except=${pathToExtension}`,
-      `--load-extension=${pathToExtension}`
-    ],
+    args: [`--disable-extensions-except=${pathToExtension}`, `--load-extension=${pathToExtension}`],
     /* "executablePath" - set by Docker container on CI, not used locally */
-    executablePath: process.env.PUPPETEER_EXEC_PATH
-  }
+    executablePath: process.env.PUPPETEER_EXEC_PATH,
+  },
 };

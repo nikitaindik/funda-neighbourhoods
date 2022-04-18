957ca9b24e164fc8d8e3bf1e453af75bf371f1c0
Nikita Indik <thevorpall@gmail.com>
2022-04-18 23:34:59 +0200

    .

--- e2e/e2e.spec.js
+++ e2e/e2e.spec.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const puppeteer = require("puppeteer-core");
 // const webExt = require("./web-ext");
+// FF silently fails with "$HOME is /github/home which is owned by uid 1001"
 const webExt = require("web-ext");
 const getPort = require("get-port");
 
